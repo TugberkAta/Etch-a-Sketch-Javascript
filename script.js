95d5d4e572a86267607e74b723cba198ab9bab5e
TugberkAta <atatugberk@yahoo.com>
2023-09-02 16:05:26 +0300

    Add dom function that lowers the opacity to half every time you hover with mouse
    Add div container and a main body container to make a site structure

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
+let height = 15
+let width = 15
 
 
 const body = document.body;
+const mainBody = document.createElement("div")
+const playField = document.createElement("div");
 const container = document.createElement("div");
-body.append(container);
-container.className="container"
+const  button = document.createElement("button")
+body.append(mainBody)
+mainBody.append(playField);
+playField.append(container);
+mainBody.append(button);
+container.className="container";
+playField.className="playField";
+mainBody.className="mainBody";
 
-for(var i = 0; i<= 15; i++){
+for(var i = 0; i<= height; i++){
     var xAxis=document.createElement("div");
     container.appendChild(xAxis);
     xAxis.className="xAxis";
-    for(var j = 0; j<= 15; j++){
-     var block=document.createElement("div");
-     xAxis.appendChild(block);
-     block.className="block";
-    }
+    for(var j = 0; j<= width; j++){
+        var block=document.createElement("div");
+        xAxis.appendChild(block);
+        block.className="block";
+
+        block.addEventListener("mouseover",(e) => {
+            e.target.style.opacity = 0.5;
+        });
+     }
  }
+
+
 
 
