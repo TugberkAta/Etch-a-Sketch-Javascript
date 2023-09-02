151515f78f88e8947e4214d711ebc0f4d1128f4b
TugberkAta <atatugberk@yahoo.com>
2023-09-02 18:22:56 +0300

    Add a button to resize the grid
    Add another button to reset the grid
    
    add a footer

--- script.js
+++ script.js
@@ -1,34 +1,74 @@
 let height = 15
 let width = 15
 
-
 const body = document.body;
 const mainBody = document.createElement("div")
 const playField = document.createElement("div");
 const container = document.createElement("div");
-const  button = document.createElement("button")
-body.append(mainBody)
+const buttonContainer = document.createElement("div")
+const buttonCreate = document.createElement("button")
+const buttonReset = document.createElement("button")
+const buttonCreateText = document.createElement("p")
+const buttonResetText = document.createElement("p")
+
+body.append(mainBody);
+mainBody.append(buttonContainer)
+buttonContainer.append(buttonCreate);
+buttonContainer.append(buttonReset);
 mainBody.append(playField);
 playField.append(container);
-mainBody.append(button);
+buttonCreate.append(buttonCreateText);
+buttonReset.append(buttonResetText)
+
+buttonCreateText.textContent="Click Here To Resize";
+buttonResetText.textContent="Click Here To Reset"
+
 container.className="container";
 playField.className="playField";
 mainBody.className="mainBody";
+buttonCreate.className = "button";
+buttonReset.className = "button";
+buttonContainer.className = "buttonContainer"
 
-for(var i = 0; i<= height; i++){
-    var xAxis=document.createElement("div");
-    container.appendChild(xAxis);
-    xAxis.className="xAxis";
-    for(var j = 0; j<= width; j++){
-        var block=document.createElement("div");
-        xAxis.appendChild(block);
+
+function createGrid(height, width){
+for(let i = 0; i< width; i++){
+    let yAxis=document.createElement("div");
+    container.appendChild(yAxis);
+    yAxis.className="yAxis";
+    for(let j = 0; j< height; j++){
+        let block=document.createElement("div");
+        yAxis.appendChild(block);
         block.className="block";
 
         block.addEventListener("mouseover",(e) => {
             e.target.style.opacity = 0.5;
         });
+
      }
- }
+}
+}
+
+createGrid(height, width)
+
+buttonCreate.addEventListener("click", (e)=>{
+    container.innerHTML= "";
+    do{
+        height = Number(prompt("Max 30\nHeight:"))
+    }while(height>=31 || height < 0)
+    do{
+        width = Number(prompt("Max 30\nWidth: "))
+    }while(width>=31 || height < 0)
+
+    createGrid(height, width)
+});
+
+buttonReset.addEventListener("click", (e)=>{
+            let blocks = document.querySelectorAll(".block")
+            blocks.forEach(block =>{
+                block.style.opacity = 1;
+            });
+        });
 
 
 
